5d1776040d702b61a5aa740d3e21b306aba4ab4a
ionide <mar109us@gmail.com>
2026-09-23 10:28:14 +0200

    init exam

---
+++ ROT-Norsk-main/logic.js
@@ -0,0 +1,13 @@
+
+function encrypt(text) {
+}
+
+function decrypt(text) {
+}
+
+function rotateText(text, shift) {
+}
+
+function rotateChar(char, shift) {
+  const alphabet = "abcdefghijklmnopqrstuvwxyzæøå";
+}
